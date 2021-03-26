4f69c5d935fc1e1b7f416798b88ff34bb7fcdc8c
jf15558 <50243608+jf15558@users.noreply.github.com>
2021-03-26 13:46:24 +0000

    Initial commit

--- R/plot_t2.R
+++ R/plot_t2.R
@@ -25,6 +25,7 @@
 #' @return A plot of the relationships of the specified
 #' elements
 #' @import igraph
+#' @export
 
 plot_t2 <- function(x, taxon, trank, ranks, mode = c("parent", "child", "all"), step = NULL) {
 
@@ -48,7 +49,7 @@ plot_t2 <- function(x, taxon, trank, ranks, mode = c("parent", "child", "all"), 
     rseq <- seq(from = -1, to = 1, length.out = length(tv$ranks))
     par(mar = c(0, 5, 0, 5))
     par(las = 1)
-    plot(tv$seq, layout = lay$layout, edge.arrow.mode = 0)
+    plot(tv$taxa, layout = lay$layout, edge.arrow.mode = 0)
     axis(4, labels = rev(names(tv$ranks)[unique(igraph::V(tv$seq)$rank[order(igraph::V(tv$seq)$rank)])]), at = rseq, col = NA)
   }
 }
