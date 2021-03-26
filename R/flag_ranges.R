096342d350e91ef6085957d16d3867101e6d9569
jf15558 <50243608+jf15558@users.noreply.github.com>
2021-03-26 18:20:02 +0000

    Initial commit

---
+++ R/flag_ranges.R
@@ -0,0 +1,252 @@
+#' flag_ranges
+#'
+#' Function to compare stratigraphic ranges in x to a
+#' set of reference ranges from y. A list of two elements
+#' is returned. The first is a dataframe summarising
+#' the overall error status, specific error counts
+#' FAD and LAD differences, and the 95% density
+#' distributions of the FAD and LAD errors for each
+#' unique taxon in the column of x denoted by the first
+#' element of xcols. If a taxon in x is not present in
+#' y, it is assigned the status 000 and its other
+#' entries in the returned dataframe will be NA.
+#' The second element of the returned list is the error
+#' code for every individual element of the column of
+#' x denoted by the first element of xcols - this will
+#' have the same number of rows as x. If x is a range
+#' table rather than an occurrence dataset, then the two
+#' list elements will have the same number of rows.
+#' Ranges for comparison may be supplied directly in y,
+#' or y may be another occurrence dataset, in which case
+#' @seealso age_ranges is called internally to generate
+#' the range table for comparison.
+#' @param x Stratigraphic range data for taxa as a whole
+#' or for individual fossil occurrences
+#' @param y The same as in x. This is the dataset to
+#' which ranges will be compared
+#' @param xcols A character vector of length three
+#' specifying, in the following order, the taxonomic name,
+#' stratigraphic base (FAD) and stratigraphic top (LAD)
+#' columns in x.
+#' @param ycols An optional character vector of length
+#' three for the same column types as in xcols, but for
+#' dataset y. This is useful if the column names differ
+#' between the datasets
+#' @param alpha A numeric of length 1, greater than 0 and
+#' less than 1 - the interval for which the error density
+#' on taxon FADs and LADs will be returned
+#' @param verbose A logical of length one determining if
+#' the flagging progress should be reported to the console
+#' @return A list of two dataframes, the first recording
+#' overall error statistics, the second recording error
+#' types for each element of x
+#' @importFrom stats density
+#' @importFrom BMS quantile.coef.density
+#' @import data.table
+#' @export
+
+flag_ranges <- function(x = NULL, y = NULL, xcols = NULL, ycols = NULL, alpha = 0.95, verbose = TRUE) {
+
+  # check all information is supplied
+  if(is.null(x) | is.null(y)) {
+    stop("Please supply both x and y arguments")
+  }
+  # if the dataframes have less than three columns supplied, break
+  if(ncol(x) < 3 | ncol(y) < 3) {
+    stop("Supplied dataframes do not contain enough data (at least name, FAD and LAD needed in both")
+  }
+  if(ncol(x) > 3 | ncol(y) > 3) {
+    if(is.null(xcols)) {
+      stop("Please supply column names (either x, y or both")
+    }
+    if(is.null(ycols)) {
+      ycols <- xcols
+    }
+  }
+  if(alpha <= 0 | alpha >= 1) {
+    stop("Alpha must be between 0 - 1, not inclusive")
+  }
+  # global variable workaround for data.table syntax
+  . <- NULL
+
+  # subselect if many columns are present
+  if(ncol(x) > 3) {
+    x <- x[,xcols]
+  }
+  if(ncol(y) > 3) {
+    y <- y[,xcols]
+  }
+  colnames(x) <- colnames(y) <- c("taxon", "max", "min")
+
+  # count unique taxon names
+  n1 <- unique(x[,"taxon"])
+  n2 <- unique(y[,"taxon"])
+
+  # derive checking range table if needed
+  if(length(n1) != nrow(x)) {
+    xr1 <- age_ranges(data = x, taxonomy = "taxon", srt = "max", end = "min")
+  } else {
+    xr1 <- x
+  }
+  xr1 <- xr1[order(xr1[,1]),]
+
+  if(length(n2) != nrow(y)) {
+    yr <- age_ranges(data = y, taxonomy = "taxon", srt = "max", end = "min")
+  } else {
+    yr <- y
+  }
+  yr <- yr[order(yr[,1]),]
+  colnames(xr1) <- colnames(yr) <- colnames(x)
+
+  # cut down to elements of xr in yr
+  xr <- xr1[xr1$taxon %in% yr$taxon,]
+  rownames(xr) <- NULL
+  # dataframe to store output
+  z <- data.frame(xr$taxon, "000", NA, NA, NA, NA, NA, NA, NA, NA)
+  colnames(z) <- c("taxon", "status", "n_1R1", "n_0R1", "n_1R0", "n_0R0", "n_00R", "n_R00", "FAD95", "LAD95")
+  flag <- rep("000", times = nrow(x))
+  fad_diff <- flag
+  fad_diff[] <- NA
+  lad_diff <- fad_diff
+
+  # convert to data.table for fast indexing
+  x$rownum <- 1:nrow(x)
+  x <- data.table::as.data.table(x)
+  data.table::setkeyv(x, "taxon")
+
+  # for each unique genus name in x
+  for(i in 1:nrow(xr)) {
+
+    # get all occurrences of the taxon
+    occs <- x[.(xr$taxon[i]),]
+    # get the matching range in the checking dataset
+    mt <- which(yr$taxon == xr$taxon[i])
+
+    # if the taxon is not present in the checking dataframe, assign skip
+    if(length(mt) == 0) {
+      1 + 1
+    } else {
+
+      z[i,3:8] <- 0
+      # if all ranges are valid
+      if(xr$max[i] <= yr$max[mt] &
+         xr$min[i] >= yr$min[mt]) {
+        flag[occs$rownum] <- "R1R"
+        z$n_1R1[i] <- nrow(occs)
+        z$status[i] <- "R1R"
+
+      } else {
+
+        # check for any valid occurrences
+        v <- occs[which(occs$max <= yr$max[mt] &
+                          occs$min >= yr$min[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "R1R"
+          z$n_1R1[i] <- nrow(v)
+          occs <- occs[-(which(occs$max <= yr$max[mt] &
+                                 occs$min >= yr$min[mt])),]
+        }
+
+        # check for occurrences which exceed FAD and LAD
+        v <- occs[which(occs$min < yr$min[mt] &
+                          occs$max > yr$max[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "0R0"
+          fad_diff[v$rownum] <- v$max - yr$max[mt]
+          lad_diff[v$rownum] <- yr$max[mt] - v$min
+          z$n_0R0[i] <- nrow(v)
+          z$status[i] <- "0R0"
+        }
+
+        # check for occurrences fully outside of the FAD or LAD
+        v <- occs[which(occs$max > yr$max[mt] &
+                          occs$min > yr$max[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "00R"
+          fad_diff[v$rownum] <- v$max - yr$max[mt]
+          z$n_00R[i] <- nrow(v)
+          if(z$status[i] == "000") {z$status[i] <- "00R"}
+        }
+        v <- occs[which(occs$min < yr$min[mt] &
+                          occs$max < yr$min[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "R00"
+          lad_diff[v$rownum] <- yr$max[mt] - v$min
+          z$n_R00[i] <- nrow(v)
+          if(z$status[i] == "000") {z$status[i] <- "R00"}
+          if(z$status[i] == "00R") {z$status[i] <- "0R0"}
+        }
+
+        # check for occurrences which exceed the FAD only
+        v <- occs[which(occs$min <= yr$max[mt] & occs$min >= yr$min[mt] &
+                          occs$max > yr$max[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "0R1"
+          fad_diff[v$rownum] <- v$max - yr$max[mt]
+          z$n_0R1[i] <- nrow(v)
+          if(z$status[i] == "000") {z$status[i] <- "0R1"}
+          if(z$status[i] == "R00") {z$status[i] <- "0R0"}
+        }
+        # check for occurrences which exceed the LAD only
+        v <- occs[which(occs$min < yr$min[mt] &
+                          occs$max >= yr$min[mt] & occs$max <= yr$max[mt]),]
+        if(nrow(v) != 0) {
+          flag[v$rownum] <- "1R0"
+          lad_diff[v$rownum] <- yr$max[mt] - v$min
+          z$n_1R0[i] <- nrow(v)
+          if(z$status[i] == "000") {z$status[i] <- "1R0"}
+          if(z$status[i] %in% c("00R", "01R")) {z$status[i] <- "0R0"}
+        }
+
+        # get the KDE for the boundaries if there are suitable errors for this
+        if(z$status[i] %in% c("0R0", "0R1", "00R")) {
+          upr <- occs[which(occs$max > yr$max[mt] | occs$min > yr$max[mt]),]
+          # add very small constant to deal with zero range ages
+          upr[,3] <- upr[,3] - 0.1
+          upr$min[which(upr$min < yr$max[mt])] <- yr$min[mt]
+          if(min(upr[,2] - upr[,3]) < 0.01) {
+            steps <- -(min(upr[,2] - upr[,3]))
+          } else {
+            steps <- -0.01
+          }
+          upr <- unlist(apply(upr, 1, function(x) {seq(from = x[2], to = x[3], by = -0.1)}))
+          ud <- stats::density(c(upr, yr$max[mt]), from = yr$max[mt], to = max(upr))
+          z$FAD95[i] <- BMS::quantile.coef.density(ud, probs = alpha)
+        }
+
+        if(z$status[i] %in% c("0R0", "1R0", "R00")) {
+          lwr <- occs[which(occs$min < yr$min[mt] | occs$max < yr$min[mt]),]
+          # add very small constant to deal with zero range ages
+          lwr[,2] <- lwr[,2] + 0.1
+          lwr$max[which(lwr$max > yr$min[mt])] <- yr$min[mt]
+          if(min(lwr[,2] - lwr[,3]) < 0.01) {
+            steps <- -(min(lwr[,2] - lwr[,3]))
+          } else {
+            steps <- -0.01
+          }
+          lwr <- unlist(apply(lwr, 1, function(x) {seq(from = x[2], to = x[3], by = steps)}))
+          ld <- stats::density(c(yr$min[mt], lwr), from = min(lwr), to = yr$min[mt])
+          z$LAD95[i] <- BMS::quantile.coef.density(ld, probs = 1 - alpha)
+        }
+      }
+    }
+    # notify R
+    if(verbose) {
+      if(i != 1) {cat(paste0("\r"))}
+      cat(paste0("Taxon ", i, "/", nrow(xr), " checked"))
+    }
+  }
+  z$y_FAD <- yr$min[match(z$taxon, yr$taxon)]
+  z$y_LAD <- yr$max[match(z$taxon, yr$taxon)]
+  z$FAD_diff <- z$FAD95 - z$y_FAD
+  z$LAD_diff <- z$y_LAD - z$LAD95
+  per_occ <- cbind.data.frame(flag, round(as.numeric(fad_diff), digits = 2),
+                              round(as.numeric(lad_diff), digits = 2))
+  colnames(per_occ) <- c("code", "fad_diff", "lad_diff")
+
+  # return
+  out <- list()
+  out[[1]] <- z
+  out[[2]] <- per_occ
+  return(out)
+}
---
+++ R/resolve_ranges.R
@@ -0,0 +1,156 @@
+#' resolve_ranges
+#'
+#' Function to generate a consensus age for assemblages
+#' of fossil data in x, given a table of taxonomic
+#' ranges. The need for error-checking is informed by
+#' the error codes for the individual fossil
+#' occurrences within each collection - if there is no
+#' error, then the consensus age is unchanged. If
+#' errors are present, then a consensus age for a
+#' threshold proportion of taxa is searched for using
+#' the overlap of the ranges for those taxa, as given
+#' in range table y. Taxa whose occurrences lie outside
+#' this consensus age are flagged as potential taxonomic
+#' errors. If the threshold consensus partially overlaps
+#' with the assemblage age, this overlap is returned to
+#' present overzealous alteration of the age - otherwise
+#' the complete consensus age is returned. If a
+#' consensus age cannot be found, the original assemblage
+#' age is returned, and each occurrence in the collection
+#' flagged as potential taxonomic errors.
+#' @param x Fossil occurrence data grouped into
+#' spatiotemporally distinct assemblages
+#' @param y A stratigraphic range dataset from which
+#' consensus assemblage ages will be derived
+#' @param assemblage The column name of the assemblage
+#' groups in x
+#' @param srt The column name of stratigraphic bases for
+#' each element in both x and y - i.e. x and y must have
+#' this same name for that column
+#' @param end The column name of stratigraphic tops for
+#' each element in both x and y - i.e. x and y must have
+#' this same name for that column
+#' @param err The column name flagging age errors for
+#' occurrences in x. Age errors can be derived using
+#' @seealso flag_ranges. Otherwise, this can be
+#' fudged by adding an error column to x which will
+#' result in each collection being assessed:
+#' `x$err <- rep("0R0, times = nrow(x))`
+#' @param taxon The column name denoting the taxon names
+#' in both x and y - i.e. x and y must have
+#' this same name for that column
+#' @param prop A numeric, between 0 and 1, denoting the
+#' threshold percentage of taxa in the assemblage for
+#' which a consensus age must be found
+#' @param verbose A logical determining if the progress
+#' of the redating procedure should be reported
+#' @return A list of two dataframes, the first recording
+#' the results of the consensus redating procedure for
+#' each assemblage in x, the second recording any flags
+#' (if any) for each occurrence in x
+#' @import data.table
+#' @importFrom stats complete.cases
+#' @export
+
+resolve_ranges <- function(x, y, assemblage = "collection_no", srt = "max_ma",
+                           end = "min_ma", err = "age_flag", taxon = "genus", prop = 0.75, verbose = TRUE) {
+
+  # global variable workaround
+  . <- lb <- ub <- b <- N <- NULL
+  # tabulate error codes in each collection (ignoring uncoded and correct ages R1R, 000)
+  codes <- c("000", "R1R", "0R0", "00R", "R00", "0R1", "1R0")
+  tabs <- data.frame(unique(x[,assemblage]), NA, NA, NA, NA, NA, NA, NA)
+  for(i in 1:length(codes)) {
+    vals <- tapply(x[,err], x[,assemblage], function(x) {length(which(x == codes[i]))})
+    tabs[,i + 1] <- vals[match(tabs$unique.x...assemblage.., as.numeric(names(vals)))]
+  }
+  colnames(tabs) <- c("assemblage", codes)
+
+  # objects to store output. This is initialised with the assemblage ages as given in the PBDB
+  # and nodes for unrevised (revision = 0) and 100% congruency (prop = 1).
+  # 1 revision = "revised", 2 revision = "unsolved"
+  z <- data.frame(tabs$assemblage, NA, NA, "valid", 1)
+  colnames(z) <- c("assemblage", "FAD", "LAD", "revision", "prop")
+  z$FAD <- x[match(tabs$assemblage, x[,assemblage]), srt]
+  z$LAD <- x[match(tabs$assemblage, x[,assemblage]), end]
+  FAD <- x[,(srt)]
+  LAD <- x[,(end)]
+  tax_flag <- FAD
+  tax_flag[] <- 0
+
+  # find assemblages with errors
+  to_do <- apply(tabs[,4:ncol(tabs)], 1, sum)
+  to_do <- tabs$assemblage[which(to_do != 0)]
+  # convert to data.table for rapid indexing
+  x$rnum <- 1:nrow(x)
+  x1 <- data.table::data.table(x)
+  data.table::setkeyv(x1, assemblage)
+
+  # for the incorrect assemblages, find the consensus if possible
+  for(i in 1:length(to_do)) {
+
+    # select assemblage occurrences and trim to unique values
+    occs <- as.data.frame(x1[.(to_do[i]),])
+    occs2 <- unique(occs[,c(taxon, srt, end)])
+    occs2 <- occs2[stats::complete.cases(occs2),]
+    zpos <- match(to_do[i], z$assemblage)
+
+    # extract range chart taxa and test for a full overlapping range
+    foo <- y[match(occs2[,taxon], y[,taxon]), c(taxon, srt, end)]
+    foo <- foo[stats::complete.cases(foo),]
+    # god-like solution from https://stackoverflow.com/questions/66754356/finding-the-overlapping-range-of-a-set-of-vectors-in-r/66758534#66758534
+    dt <- data.table(lb = foo[[3]], ub = foo[[2]])
+    mdt <- dt[, .(b = unique(unlist(data.table::.SD)))]
+    sol <- dt[mdt, on = .(lb <= b, ub >= b), data.table::.N, by = data.table::.EACHI]
+    tprop <- max(sol$N) / nrow(foo)
+
+    # if the returned interval covers the proportion of taxa prop
+    # (N = 1 means that there are no overlaps between any ranges)
+    if(tprop >= prop) {
+
+      # get the old age boundary
+      ores <- as.vector(z[zpos, c("FAD", "LAD")])
+      # get the assemblage span
+      res <- rev(sol[N == max(N), range(lb)])
+      # take the overlap if present, otherwise take the fully revised age
+      sol2 <- rbind(ores, res)
+      sol2 <- sol2[order(sol2[,1], decreasing = TRUE),]
+      if(sol2[2,1] >= sol2[1,2]) {
+        sol2 <- c(min(sol2[,1]), max(sol2[,2]))
+      } else {
+        sol2 <- res
+      }
+      # update assemblage age and revision status
+      z[zpos, c("FAD", "LAD")] <- sol2
+      z[zpos, "revision"] <- "revised"
+      z[zpos, "prop"] <- tprop
+      FAD[occs$rnum] <- sol2[1]
+      LAD[occs$rnum] <- sol2[2]
+
+      # test for occurrences to flag
+      if(tprop != 1) {
+        foo2 <- foo[which((foo$max_ma > res[1] & foo$min_ma >= res[1]) |
+                            (foo$max_ma <= res[2] & foo$min_ma < res[2])),taxon]
+        tax_flag[occs$rnum[which(occs[,taxon] %in% foo2)]] <- 1
+      }
+
+    } else {
+      z[zpos, "prop"] <- tprop
+      z[zpos, "revision"] <- "unresolved"
+      tax_flag[occs$rnum] <- 1
+    }
+    # notify R
+    if(verbose) {
+      if(i != 1) {cat(paste0("\r"))}
+      cat(paste0("Assemblage ", i, "/", length(to_do), " checked"))
+    }
+  }
+
+  # return
+  per_occ <- cbind.data.frame(FAD, LAD, tax_flag)
+  colnames(per_occ) <- c("FAD", "LAD", "tax_flag")
+  out <- list()
+  out[[1]] <- z
+  out[[2]] <- per_occ
+  return(out)
+}
