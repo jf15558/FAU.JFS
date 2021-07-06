d18d7fd18e44584c3526fbb683e8a8764b807fbd
jf15558 <50243608+jf15558@users.noreply.github.com>
2021-07-06 01:02:36 +0100

    Initial commit

--- R/check_taxonomy.R
+++ R/check_taxonomy.R
@@ -1,14 +1,19 @@
 #' check_taxonomy
 #'
 #' Function to implement a multi-step cleaning routine for
-#' hierarchically structured taxonomic data. The first
+#' hierarchically structured taxonomic data. The first part
+#' of the routine performs a few presumptive checks on all
+#' columns, scanning for non letter characters and checking
+#' the number of words in each string (all assumed to contain
+#' 1 word, aside from species names). If chosen, @seealso
+#' clean_name is called to ensure correct formatting. The second
 #' part of the routine calls @seealso spell_check to flag
-#' spelling errors within a given taxonomic group. The second
+#' spelling errors within a given taxonomic group. The third
 #' part of the routine calls @seealso check_ranks to flag
 #' name re-use at different levels. Some of these cases may
 #' arise when a name has been unfortunately, but validly
 #' used to refer to different groups at different taxonomic
-#' levels. The third part of the routine calls @seealso
+#' levels. The fourth part of the routine calls @seealso
 #' find_duplicates to flag variable higher classifications for
 #' a given taxon. The function flags errors by default, but can
 #' return a fully cleaned taxonomic scheme with conflicting
@@ -26,11 +31,49 @@
 #' @param ranks The column names of the taxonomic data fields
 #' in x. These must be provided in order of decreasing taxonomic
 #' rank
+#' @param species A logical indicating if x contains a species
+#' column. As the data must be supplied in hierarchical order,
+#' this column will naturally be the last column in x and
+#' species-specific spell checks will be performed on this column.
 #' @param routine A character vector determining the flagging
-#' and cleaning routines to employ. Valid values are spell_check
+#' and cleaning routines to employ. Valid values are format_check (check for
+#' non letter characters and the number of words in names), spell_check
 #' (flag potential spelling errors), discrete_rank (check that
 #' taxonomic names are unique to their rank), duplicate_tax (flag
 #' conflicting higher classifications of a given taxon)
+#' @param format_clean If TRUE, the function will return cleaned versions of the columns in x
+#' using the routines in @seealso clean_name. These routines can be
+#' altered using the 'terms' and 'collapse' arguments.
+#' @param spell_clean If TRUE, the function will return a cleaned version
+#' of the supplied taxonomic dataframe, using the supplied threshold
+#' for the similarity method given by method2, to automatically update
+#' any names in pairs of flagged synonyms to the more frequent spelling.
+#' This is not recommended, however, so the argument is FALSE by default
+#' and the threshold left as NULL
+#' @param thresh The threshold for the similarity method given by method2,
+#' below which flagged pairs of names will be considered synonyms and
+#' resolved automatically. See @seealso spell_check for details on method2
+#' @param tax_clean If TRUE, the function will return a cleaned version
+#' of the supplied taxonomic dataframe, using @seealso resolve_duplicates
+#' to resolve conflicts in the way documented by the function. Both
+#' spell_clean and tax_clean can both be TRUE to return a dataset cleaned
+#' by both methods
+#' @param append If TRUE, any suffixes used during cleaning will
+#' be retained in the cleaned version of the data. This is preferable
+#' as it ensures that all taxonomic names are rank-discrete and
+#' uniquely classified
+#' @param terms If not NULL, a character vector of terms (which
+#' will be used at all ranks) or a list of rank-specific terms to be cleaned
+#' from taxonomic names if format_clean = TRUE. If a list, this
+#' should be given in descending rank order. Called by @seealso
+#' clean_name
+#' @param collapse If not NULL, a character vector of string (which
+#' will be used at all ranks) or a list of rank-specific strings which will
+#' be collapsed in taxonomic names if format_clean = TRUE (i.e. replaced by "",
+#' rather than the default " "). If one of the collapse terms is a regex special
+#' character, it will need to be escaped, e.g. "\-". If a list, this
+#' should be given in descending rank order. Called by @seealso
+#' clean_name
 #' @param jw a numeric greater than 0 and less than 1. This is
 #' the distance threshold below which potential synonyms will be
 #' considered. Called by @seealso spell_check
@@ -72,36 +115,19 @@
 #' Called by @seealso resolve_duplicates
 #' @param plot A logical speciying if conflicting classifications should
 #' be plotted. Called by @seealso resolve_duplicates
-#' @param spell_clean If TRUE, the function will return a cleaned version
-#' of the supplied taxonomic dataframe, using the supplied threshold
-#' for the similarity method given by method2, to automatically update
-#' any names in pairs of flagged synonyms to the more frequent spelling.
-#' This is not recommended, however, so the argument is FALSE by default
-#' and the threshold left as NULL
-#' @param thresh The threshold for the similarity method given by method2,
-#' below which flagged pairs of names will be considered synonyms and
-#' resolved automatically. See @seealso spell_check for details on method2
-#' @param tax_clean If TRUE, the function will return a cleaned version
-#' of the supplied taxonomic dataframe, using @seealso resolve_duplicates
-#' to resolve conflicts in the way documented by the function. Both
-#' spell_clean and tax_clean can both be TRUE to return a dataset cleaned
-#' by both methods
-#' @param append If TRUE, any suffixes used during cleaning will
-#' be retained in the cleaned version of the data. This is preferable
-#' as it ensures that all taxonomic names are rank-discrete and
-#' uniquely classified
 #' @return A list with elements corresponding to the outputs of the
 #' chosen flagging routines (three by default), plus
-#' a cleaned taxonomic dataframe if either spell_clean or tax_clean or both
+#' a cleaned taxonomic dataframe if any of format_clean, spell_clean or tax_clean
 #' are TRUE. See @seealso spell_clean, @seealso check_ranks and @seealso
 #' find_duplicates for details of the structure of the outputs
 #' @importFrom stats na.omit
 #' @export
 
-check_taxonomy <- function(x, ranks = NULL, routine = c("spell_check", "discrete_rank", "duplicate_tax"),
+check_taxonomy <- function(x, ranks = NULL, species = FALSE, routine = c("format_check", "spell_check", "discrete_rank", "duplicate_tax"),
+                           format_clean = FALSE, spell_clean = FALSE, thresh = NULL, tax_clean = TRUE, append = TRUE,
+                           terms = NULL, collapse = NULL,
                            jw = 0.1, str = 1, str2 = NULL, method2 = "jaccard", q = 1, pref = NULL, suff = NULL, exclude = NULL,
-                           jump = 3, plot = FALSE,
-                           spell_clean = FALSE, thresh = NULL, tax_clean = TRUE, append = TRUE) {
+                           jump = 3, plot = FALSE) {
 
   # check that data has minimally been supplied
   if(!exists("x")) {
@@ -128,23 +154,67 @@ check_taxonomy <- function(x, ranks = NULL, routine = c("spell_check", "discrete
   if(!all(apply(x, 2, class) == "character")) {
     stop("Not all columns in x are of class character")
   }
+  # check species designator
+  if(!is.logical(species) & length(species) != 1) {
+    stop("Species should be a logical of length one, indicating whether species-level designations are present in x")
+  }
 
   # check cleaning routines have been correctly supplied
   if(!is.character(routine)) {
     stop("Routine should be a character vector containing one or more of the following:
-         spell_check, discrete_rank, report_tax, resolve_tax")
+         format_check, discrete_rank, report_tax, resolve_tax")
   }
-  if(!any(routine %in% c("spell_check", "discrete_rank", "duplicate_tax"))) {
+  if(!any(routine %in% c("clean_name", "spell_check", "discrete_rank", "duplicate_tax"))) {
     stop("All elements of argument routine are invalid.
-         Valid elements are spell_check", "discrete_rank", "duplicate_tax")
+         Valid elements are format_check, spell_check", "discrete_rank", "duplicate_tax")
   }
-  if(!all(routine %in% c("spell_check", "discrete_rank", "duplicate_tax"))) {
+  if(!all(routine %in% c("format_check", "spell_check", "discrete_rank", "duplicate_tax"))) {
     warning("Some elements of argument routine are invalid and will be ignored.
-            Valid elements are spell_check", "discrete_rank", "duplicate_tax")
+            Valid elements are format_check, discrete_rank, report_tax, resolve_tax")
   }
   # ensure routine vector is clean and correctly ordered
   routine <- unique(routine)
   routine <- as.vector(na.omit(routine[match(c("spell_check", "discrete_rank", "duplicate_tax"), routine)]))
+
+  # check additional flags
+  if(format_clean) {
+
+    if(is.null(terms)) {
+      terms <- as.list(1:ncol(x))
+      terms_list <- lapply(terms, function(x) {x <- NULL})
+    } else {
+      if(is.atomic(terms)) {
+        terms_list <- lapply(1:ncol(x), function(x) {x <- terms})
+      }
+      if(is.list(terms)) {terms_list <- terms}
+      if(length(terms_list) != length(ranks)) {
+        stop("Terms should be supplied either as a vector which will be used at all taxonomic levels, or as a list of
+             vectors to be used at each specific level in x (length must equal number of columns in x/number of ranks")
+      }
+      if (!all(unlist(lapply(terms_list, class)) %in% c("NULL", "character"))) {
+        stop("Not all elements of terms are of class character")
+      }
+      terms_list <- lapply(terms_list, function(x) {as.vector(na.omit(x))})
+    }
+
+    if(is.null(collapse)) {
+      collapse <- as.list(1:ncol(x))
+      collapse_list <- lapply(collapse, function(x) {x <- NULL})
+    } else {
+      if(is.atomic(collapse)) {
+        collapse_list <- lapply(1:ncol(x), function(x) {x <- collapse})
+      }
+      if(is.list(collapse)) {terms_list <- collapse}
+      if(length(collapse_list) != length(ranks)) {
+        stop("Collapse should be supplied either as a vector which will be used at all taxonomic levels, or as a list of
+             vectors to be used at each specific level in x (length must equal number of columns in x/number of ranks")
+      }
+      if (!all(unlist(lapply(collapse_list, class)) %in% c("NULL", "character"))) {
+        stop("Not all elements of collapse are of class character. Additionally, any regex special characters must be escaped using backslashes")
+      }
+      terms_list <- lapply(terms_list, function(x) {as.vector(na.omit(x))})
+    }
+  }
 
   # check additional flags
   if("spell_check" %in% routine) {
@@ -226,6 +296,46 @@ check_taxonomy <- function(x, ranks = NULL, routine = c("spell_check", "discrete
   # set up output list
   out <- list()
 
+  # check formatting
+  if("format_check" %in% routine) {
+
+    chars <- vector()
+    for(i in 1:length(ranks)) {
+      chars[i] <- any(grepl("[^[:alpha:]]", x[,ranks[i]]))
+    }
+    if(sum(chars) != 0) {
+      warnings(paste0("Non-letter characters detected at the following ranks: ", paste0(ranks[chars], collapse = ", ")))
+    }
+
+    if(species) {
+      lens <- vector()
+      for(i in 1:(length(ranks) - 1)) {
+        lens[i] <- any(as.logical(unlist(lapply(strsplit(x[,ranks[i]], " "), length)) - 1))
+      }
+      if(sum(lens) != 0) {
+        warning(paste0("The following ranks contain names consisting of more than one word: ", paste0(ranks[lens], collapse = ", "),  ". Supraspecific taxon names are assumed to consist of single words"))
+      }
+      if(any(as.logical(unlist(lapply(strsplit(x[,ranks[length(ranks)]], " "), length)) - 2))) {
+        warning(paste0("The species colum contain names consisting of more than two words: ", paste0(ranks[lens], collapse = ", "),  ". Species names are assumed to consist of no more than two words"))
+      }
+
+    } else {
+      lens <- vector()
+      for(i in 1:length(ranks)) {
+        lens[i] <- any(as.logical(unlist(lapply(strsplit(x[,ranks[i]], " "), length)) - 1))
+      }
+      if(sum(lens) != 0) {
+        warnings(paste0("The following ranks contain names consisting of more than one word: ", paste0(ranks[lens], collapse = ", "),  ". Supraspecific taxon names are assumed to consist of single words"))
+      }
+    }
+
+    if(format_clean) {
+      for(i in 1:length(ranks)) {
+        x[,i] <- clean_name(x[,i], terms = terms_list[[i]], collapse = collapse_list[[i]])
+      }
+    }
+  }
+
   # check spelling
   if("spell_check" %in% routine) {
 
@@ -271,7 +381,7 @@ check_taxonomy <- function(x, ranks = NULL, routine = c("spell_check", "discrete
   }
 
   # format output
-  if(tax_clean | spell_clean) {
+  if(format_clean | tax_clean | spell_clean) {
     if(!append) {
       # remove the duplicate classification suffixes
       x <- apply(x, 2, function(y) {gsub("[A-Z]$", "", x = y)})
